ce9884363ba6289cbc70852bf5ddf4069f661ea5
Pedro Gonzalez <orion@Pedros-MacBook-Pro.local>
2024-01-18 01:42:45 -0600

    this contains updated code in the JS script that fixes some of the bugs encountered due to typos and minor errors in naming some of the attributes.

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,59 @@
-var timerElement = document.querySelector(".timer-count");
-var startButton = document.querySelector(".start-button");
-var questions = document.querySelector("section")
+// The following code builds the questions that are being asked during the quiz.
+// They indicate question
 
-var timer;
-var timerCount;
+let questions = [
+    {
+        question: "Commonly used data types DO NOT include:",
+        options: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+        answer: "alerts",
+    },
+    {
+        question: "The condition in an if/else statement is enclosed within __________.",
+        options: ["1. quotes", "2. curlybrackets", "3. paranthesis", "4. square brackets"],
+        answer: "paranthesis",
+    },
+    {
+        question: "Arrays in JavaScript can be used to store ________.",
+        options: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
+        answer: "all of the above",
+    },
+    {
+        question: "String values must be enclosed within ______ when being assigned to variables.",
+        options: ["1. commas", "2. curly brackets", "3. quotes", "4. paranthesis"],
+        question: "commas",
+    },
+    {
+        question: "A very useful tool used during development and debuggin for printing content to the debugger is:",
+        options: ["1. JavaScript", "2. terminal/bash", "3. for loops", "4. console.log"],
+        answer: "console.log",
+    },
+    {
+        question: "String values must be enclosed within ______ when being assigned to variables.",
+        options: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+        answer: "alerts",
+    },
+];
 
 // Get Dom Elements
 let questionsEl = document.querySelector("#questions");
 let timerCountEl = document.querySelector("#timer-count");
 let choicesEl = document.querySelector("#options");
 let submitBtn = document.querySelector("#submit-score");
-let startBtn = document.querySelector("#start-button");
+let startBtn = document.querySelector(".start-button");
 let nameEl = document.querySelector("#name");
 let feedbackEl = document.querySelector("#feedback");
 let restartBtn = document.querySelector("#restart");
 
 // Before quiz begins
 let currentQuestionIndex = 0;
-let time = questions.lenght * 15;
+let time = questions.length * 15;
 let timerCount;
 
 // The startGame function is called when the start button is clicked. Quiz starts and hides beginning page.
 function startQuiz(){
     timerCount = setInterval(clockTick, 1000);
-    timerElement.textContent = time;
+    timerCountEl.textContent = time;
+
     let landingScreenEl = document.getElementById("start-screen");
     landingScreenEl.setAttribute("class", "hide");
     questionsEl.removeAttribute("class");
@@ -34,17 +64,28 @@ function startQuiz(){
 function getQuestions(){
     let currentQuestion = questions[currentQuestionIndex];
     let promptEl = document.getElementById("questions-words");
-    promptEl.textContent = currentQuestion.prompt;
-    choicesEl.innerHTML = "";
-    currentQuestion.options.forEach(
-        function (choice, i) {
-            let choiceBtn = document.createElement("button");
-            choiceBtn.setAttribute("value", choice);
-            choiceBtn.textContent = i + 1 + ". " choice;
-            choiceBtn.onclick = questionClick;
-            choicesEl.appendChild(choiceBtn);
+    
+    if (promptEl){
+        promptEl.textContent = currentQuestion.question;
+        
+        let choicesEl = document.getElementById("options");
+
+        if (choicesEl){
+            choicesEl.innerHTML = "";
         }
-    );
+        
+            currentQuestion.options.forEach(function (choice, i) {
+                let choiceBtn = document.createElement("button");
+                choiceBtn.setAttribute("value", choice);
+                choiceBtn.textContent = i + 1 + ". " + choice;
+                
+                if(choiceBtn && choicesEl){
+                    choiceBtn.onclick = questionClick;
+                    choicesEl.appendChild(choiceBtn); 
+                }    
+            }
+        );
+    }
 }
 
 //This checks for correct answers and deducts time for wrong answer and goes to the next questions
@@ -55,7 +96,7 @@ function questionClick(){
             time = 0;
         }
         timerCountEl.textContent = time;
-        feedbackEl.textContent = 'Wrong! The correct answer was ${questions[currentQuestionIndex].answer}.';
+        feedbackEl.textContent = `Wrong! The correct answer was ${questions[currentQuestionIndex].answer}.`;
         feedbackEl.style.color = "red";
     } else {
         feedbackEl.textContent = "Correct!";
@@ -64,9 +105,9 @@ function questionClick(){
     feedbackEl.setAttribute("class", "feedback");
     setTimeout(function(){
         feedbackEl.setAttribute("class", "feedback hide");
-    }, 2000);
+    }, 1000);
     currentQuestionIndex++;
-    if(currentQuestionIndex === questions.lenght){
+    if(currentQuestionIndex === questions.length){
         quizEnd();
     } else{
         getQuestions();
@@ -75,7 +116,7 @@ function questionClick(){
 
 //Ends the quiz by hiding the questions and stops time to show final score.
 function quizEnd(){
-    clearInterval(timerCountId);
+    clearInterval(timerCount);
     let endScreenEl = document.getElementById("quiz-end");
     endScreenEl.removeAttribute("class");
     let finalScoreEl = document.getElementById("score-final");
@@ -95,7 +136,7 @@ function clockTick(){
 //This saves the scores to the local storage along with name.
 function saveHighscore(){
     let name = nameEl.value.trim();
-    if(name = !-- ""){
+    if(name !== "") {
         let highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
         let newScore = {
             score: time,
@@ -107,69 +148,17 @@ function saveHighscore(){
     }
 }
 
+//This is to save the users' score after pressing enter.
+function checkForEnter(event){
+    if (event.key === "Enter"){
+        saveHighscore();
+        alert("Your score has been submitted!");
+    }
+}
 
-// The following code builds the questions that are being asked during the quiz.
-// They indicate question
+nameEl.onkeyup = checkForEnter;
 
-Let questions = [
-    {
-        prompt: "Commonly used data types DO NOT include:"
-        options: [
-            "1. strings",
-            "2. booleans",
-            "3. alerts",
-            "4. numbers",
-        ]
-        answer: "alerts",
-    },
-    {
-        prompt: "The condition in an if/else statement is enclosed within __________."
-        options: [
-            "1. quotes",
-            "2. curlybrackets",
-            "3. paranthesis",
-            "4. square brackets",
-        ]
-        answer: "paranthesis",
-    },
-    {
-        prompt: "Arrays in JavaScript can be used to store ________."
-        options: [
-            "1. numbers and strings",
-            "2. other arrays",
-            "3. booleans",
-            "4. all of the above",
-        ]
-        answer: "all of the above",
-    },
-    {
-        prompt: "String values must be enclosed within ______ when being assigned to variables."
-        options: [
-            "1. commas",
-            "2. curly brackets",
-            "3. quotes",
-            "4. paranthesis",
-        ]
-        answer: "commas",
-    },
-    {
-        prompt: "A very useful tool used during development and debuggin for printing content to the debugger is:"
-        options: [
-            "1. JavaScript",
-            "2. terminal/bash",
-            "3. for loops",
-            "4. console.log",
-        ]
-        answer: "console.log",
-    },
-    {
-        prompt: "String values must be enclosed within ______ when being assigned to variables."
-        options: [
-            "1. strings",
-            "2. booleans",
-            "3. alerts",
-            "4. numbers",
-        ]
-        answer: "alerts",
-    },
-];
+submitBtn.onclick = saveHighscore;
+
+startBtn.onclick = startQuiz;
+
