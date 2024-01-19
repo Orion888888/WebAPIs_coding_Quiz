f6b7eee8fe239bb4e9cac663daab00cd81674f61
Pedro Gonzalez <orion@Pedros-MacBook-Pro.local>
2024-01-18 18:56:23 -0600

    attempted to further debug the application to get the score list to append as an ordered list, but it only stores the list into the local storage.

--- assets/highscore.js
+++ assets/highscore.js
@@ -3,13 +3,17 @@ let scoresBtn = document.querySelector("#view-scoreboard");
 //This is to rank the score in order by retrieving the scores from the localStorage.
 function printHighscores(){
     let highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+    console.log(highscores);
+    
     highscores.sort(function(a, b){
         return b.score - a.score;
     });
-    highscores.forEach(function(score){
+    highscores.forEach(function(score) {
         let liTag = document.createElement("li");
         liTag.textContent = score.name + " - " + score.score;
+        
         let olEl = document.getElementById("highscores");
+        
         if(olEl){
             olEl.appendChild(liTag);
         }
